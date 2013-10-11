0cb90151b7748c144eb8b766b056abe319956290
Earlz <earlz@lastyearswishes.com>
2013-10-11 17:29:35 -0400

    Hey I'm doing it! It's happening!

--- proto/protomvc.rs
+++ proto/protomvc.rs
@@ -1,7 +1,12 @@
-
+use std::os::getenv;
 
 
 fn main() {
     print("content-type: text/plain\r\n\r\n");
-    println("hello?");
+    print("hello from ");
+    let path = match getenv("PATH_INFO") {
+        Some(m) => m,
+        None => ~"you didn't specify a path :("
+    };
+    println(path);
 }
